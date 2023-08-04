d92964eca6aa7066c82f6965bcb1c61097476e02
AKannappan(AK) <76928574+ak-spec@users.noreply.github.com>
2023-08-04 09:20:00 +0800

    Update deploy.js

--- crowdfundHardhat/scripts/deploy.js
+++ crowdfundHardhat/scripts/deploy.js
@@ -1,9 +1,7 @@
 const {ethers, run, network} = require("hardhat")
 
 // hardhat_contract_address: 0x5FbDB2315678afecb367f032d93F642f64180aa3
-// sepolia_contract_address: 0x2943478e845c240b81B4601d474F460f818243E3
-
-// 0x15663148Ae4f2b0b2f52C5Ef25f1ec6CC53Ed9E6
+// sepolia_contract_address: 0x15663148Ae4f2b0b2f52C5Ef25f1ec6CC53Ed9E6
 
 async function verify(contractAddress, args) {
     try{
